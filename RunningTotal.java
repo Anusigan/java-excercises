5bf2f2cd33e91afeaae65c5af2c3702bf480bad0
Anusigan Sivananthan <sivananthananusigan@gmail.com>
2024-07-20 10:57:46 +0530

    java excercise

---
+++ RunningTotal.java
@@ -0,0 +1,16 @@
+public class RunningTotal{
+    public static int runningtotal=0;
+    public static void main(String[] args){
+        System.out.println("Running total is: "+runningtotal);
+        runningtotal=runningtotal+5;
+        System.out.println("Running total is: "+runningtotal);
+        runningtotal=runningtotal+8;
+        System.out.println("Running total is: "+runningtotal);
+        runningtotal=runningtotal+2;
+        System.out.println("Runing total is: "+runningtotal);
+        runningtotal=runningtotal+3;
+        System.out.println("Running total is: "+runningtotal);
+
+
+    }
+}
