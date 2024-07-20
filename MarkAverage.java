137a7b8d0ecf187af136f8639a2ff3f566e98dc3
Anusigan Sivananthan <sivananthananusigan@gmail.com>
2024-07-20 11:01:42 +0530

    java excercise

---
+++ MarkAverage.java
@@ -0,0 +1,10 @@
+public class MarkAverage {
+    public static void main(String[] args){
+        double mark1=80.4;
+        double mark2=60.2;
+        double mark3=20.9;
+        double average=mark1+mark2+mark3;
+        System.out.println("The average is: "+average);
+    }
+    
+}
