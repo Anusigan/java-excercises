107ccccc0a91773f03587ae6f5af923c5c83f012
Anusigan Sivananthan <sivananthananusigan@gmail.com>
2024-07-21 22:32:38 +0530

    java excerises

---
+++ method.java
@@ -0,0 +1,13 @@
+public class method {
+    public static void main(String[] args){
+        PrintNum();
+
+    }
+
+    public static void PrintNum(){
+        for(int i=0;i<=100;i++){
+            System.out.println(i);
+        }
+    }
+    
+}
