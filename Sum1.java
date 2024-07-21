aee614f2d0f2ea257886aed5d3ac37bf83ccf078
Anusigan Sivananthan <sivananthananusigan@gmail.com>
2024-07-21 22:22:52 +0530

    java excercises

---
+++ Sum1.java
@@ -0,0 +1,12 @@
+public class Sum1 {
+    public static void main(String[] args){
+        int count=1;
+        int sum=0;
+        while(count<=10){
+            sum+=count;
+            count++;
+        }
+        System.out.println("The sum is: "+sum);
+    }
+    
+}
