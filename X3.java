0fef91a96bb17d245483d361692e34f32212ca00
Anusigan Sivananthan <sivananthananusigan@gmail.com>
2024-07-23 10:25:03 +0530

    java excercises

---
+++ X3.java
@@ -0,0 +1,14 @@
+public class X3 {
+    public static void main(String[] args) {
+        int count=0;
+        do{
+            System.out.println(count);
+            count++;
+        }
+        while(count<100);{
+            
+
+        }
+    }
+    
+}
