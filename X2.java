de9b0ec92ab6b3a488d8de2836990d6c21bababd
Anusigan Sivananthan <sivananthananusigan@gmail.com>
2024-07-23 10:13:51 +0530

    java excercises

---
+++ X2.java
@@ -0,0 +1,10 @@
+public class X2 {
+    public static void main(String[] args) {
+        int count=0;
+        while(count<100){
+            System.out.println(count);
+            count++;
+        }
+    }
+    
+}
