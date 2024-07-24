6b3d9cc66a62520951ee16482d8229a3b5011cab
Anusigan Sivananthan <sivananthananusigan@gmail.com>
2024-07-24 21:01:48 +0530

    java excercises

---
+++ Exam4.java
@@ -0,0 +1,15 @@
+public class Exam4 {
+    
+    public static int math(int x,int y){
+        while(x>=y)
+            x=x-1;
+            return x;
+        
+
+    }
+    public static void main(String[] args) {
+        int i=3;
+       System.out.println(math (i,i));
+     }
+    
+}
