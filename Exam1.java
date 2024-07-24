cd1d9bb70156e6f2ecf8e570ce9180b38bdd5417
Anusigan Sivananthan <sivananthananusigan@gmail.com>
2024-07-24 20:41:41 +0530

    java excercises

---
+++ Exam1.java
@@ -0,0 +1,11 @@
+public class Exam1 {
+    public static void main(String[] args) {
+        
+    for(int x=0;x<3;x++){
+        for(int y=0;y<x;y++){
+            System.out.println(x);
+        }
+    }
+    
+}
+}
