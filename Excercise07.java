7e3a586c797825ca1b11d2f2b5c3c29da5528b49
Anusigan Sivananthan <sivananthananusigan@gmail.com>
2024-07-24 13:58:00 +0530

    java excercises

---
+++ Excercise07.java
@@ -0,0 +1,13 @@
+public class Excercise07 {
+    public static void main(String[] args) {
+        for(int i=0;i<100;i++){
+            if(i==50||i==55){
+                continue;
+            }
+            else{
+                System.out.println(i);
+            }
+        }
+    }
+    
+}
