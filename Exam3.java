66c071ba2928ac4bdb8686ab6f8c9588e616140c
Anusigan Sivananthan <sivananthananusigan@gmail.com>
2024-07-24 20:56:23 +0530

    java excercises

---
+++ Exam3.java
@@ -0,0 +1,15 @@
+public class Exam3 {
+    public static int dif(int x,int y){
+        if(x<y){
+            return y-x;
+        }
+        else{
+            return x-y;
+        }
+
+    }
+    public static void main(String[] args) {
+       System.out.println(dif(3,5));
+    }
+    
+}
