148a6948f2ff36d3691482a350554104e9881610
Anusigan Sivananthan <sivananthananusigan@gmail.com>
2024-07-27 23:38:24 +0530

    java excercises

--- Exam12.java
+++ Exam12.java
@@ -4,7 +4,10 @@ public class Exam12 {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter your first  name:");
         String name=sc.nextLine();
-        
+        int count=0;
+        while(count<name.length()){
+            System.out.print("*");
+        }
     }
 
     
