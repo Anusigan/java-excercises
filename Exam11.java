609601ea8540fe6654ba3c2eb8f12c3b7e02f29d
Anusigan Sivananthan <sivananthananusigan@gmail.com>
2024-07-27 23:22:18 +0530

    java excercises

--- Exam11.java
+++ Exam11.java
@@ -1,6 +1,12 @@
 import java.util.*;
 public class Exam11 {
+    public static void main(String[] args) {
+        
+    
     Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the name: ");
+    String name=sc.nextLine();
     
     
+}
 }
