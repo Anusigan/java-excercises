2d04bf1db7aa4bef7edb392824f4ae8c1fd9f0f7
Anusigan Sivananthan <sivananthananusigan@gmail.com>
2024-07-27 23:20:31 +0530

    java excercises

---
+++ Exam10.java
@@ -0,0 +1,12 @@
+public static void printDigits(int n){
+    if(n<10){
+        System.out.println(n+" ");
+    }
+    else{
+        printDigits(n/10);
+        System.out.println(n%10+" ");
+    }
+    public static void main(String[] args){
+        printDigits(543);
+    }
+}
---
+++ Exam11.java
@@ -0,0 +1,6 @@
+import java.util.*;
+public class Exam11 {
+    Scanner sc=new Scanner(System.in);
+    
+    
+}
