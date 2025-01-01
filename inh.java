6adf76b6797ebed7bde94891b0dfda69e2fe8e1e
Anusigan Sivananthan <148673260+Anusigan@users.noreply.github.com>
2025-01-01 16:11:48 +0530

    demo

--- inh.java
+++ inh.java
@@ -1,21 +1,27 @@
-class A{
-    public A(){
+class A  extends Object{
+    public A() {
         System.out.println("Donkey");
+    }
+
+    public A(int n) {
+        System.out.println("Ok fine");
     }
 
 }
 
-class B extends A{
-    public B (){
+class B extends A {
+    public B() {
         System.out.println("Hi");
     }
+
     public B (int n){
+        super (n);
         System.out.println("int B int");
     }
 
 }
 
-public class inh{
+public class inh {
     public static void main(String[] args) {
         B obj = new B(5);
     }
---
+++ obj.java
@@ -0,0 +1,19 @@
+class A{
+    public  A(){
+
+    }
+    public  void show(){
+        System.out.println("Hi");
+
+    }
+
+}
+
+
+public class obj {
+    public static void main(String[] args) {
+        A obj=new A();
+        obj.show();
+    }
+    
+}
