453a62f8c265bae7ec2cd92b515d8da0ffc8b920
Anusigan Sivananthan <148673260+Anusigan@users.noreply.github.com>
2025-01-01 16:44:00 +0530

    calc

---
+++ calc.java
@@ -0,0 +1,11 @@
+class calculator{
+    public int add(int n1, int n2){
+        int sum=n1+n2;
+        return(sum);
+
+    }
+    public int sub(int n1, int n2){
+        int sub=n1-n2;
+        return(sub);
+    }
+}
