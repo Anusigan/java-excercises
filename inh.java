c638eae01cf705fa42ec25a7d5f20e8437f2c42c
Anusigan Sivananthan <148673260+Anusigan@users.noreply.github.com>
2025-01-01 15:31:51 +0530

    practice 1

---
+++ inh.java
@@ -0,0 +1,16 @@
+class A{
+
+}
+
+class B extends A{
+    public B (){
+        System.out.println("Hi");
+    }
+
+}
+
+public class inh{
+    public static void main(String[] args) {
+        B obj = new B();
+    }
+}
