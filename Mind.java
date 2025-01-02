baebeb36796bbc5117cf666096e9e8c7d31efb33
Anusigan Sivananthan <148673260+Anusigan@users.noreply.github.com>
2025-01-02 23:59:53 +0530

    change

--- Mind.java
+++ Mind.java
@@ -1,17 +1,18 @@
 class Hi {
-public void Donkey(int n){
-    int add =n*2;
-    System.out.println(add);
-}
-}
-
-class Bye extends Hi{
-
-    
+    public void Donkey(int n) {
+        int add = n * 2;
+        System.out.println(add);
+    }
 }
 
+class Bye extends Hi {
+public void monkey
+    {
+
+    }
+
+}
 
 public class Mind {
 
-    
 }
