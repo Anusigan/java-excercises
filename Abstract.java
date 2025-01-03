8fc6d3a240522097b4f8aaec07e0a8a2da9bccaa
Anusigan Sivananthan <148673260+Anusigan@users.noreply.github.com>
2025-01-03 17:26:51 +0530

    abstract keyword practice

---
+++ Abstract.java
@@ -0,0 +1,22 @@
+class Car{
+    public void drive(){
+        System.out.println("Auto Driving");
+
+    }
+    public void skate(){
+        System.out.println("Play Music");
+    }
+}
+
+
+
+
+
+public class Abstract {
+    public static void main(String[] args) {
+        Car sc=new Car();
+        sc.drive();
+        sc.skate();
+    }
+    
+}
