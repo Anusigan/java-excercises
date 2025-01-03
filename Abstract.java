cd025648594a0ec2972c76b5b40cf3e323051b9d
Anusigan Sivananthan <148673260+Anusigan@users.noreply.github.com>
2025-01-03 17:31:37 +0530

    abstract keyword studying

--- Abstract.java
+++ Abstract.java
@@ -1,21 +1,23 @@
-class Car{
-    public void drive(){
-        //not implementing this method
-    }
-    public void skate(){
+class Car {
+    public void drive();
+
+    // not implementing this method
+    public void skate() {
         System.out.println("Play Music");
     }
 }
 
-
-
-
+class WagonR extends Car {
+    public void drive() {
+        System.out.println("This car will drive");
+    }
+}
 
 public class Abstract {
     public static void main(String[] args) {
-        Car sc=new Car();
+        Car sc = new Car();
         sc.drive();
         sc.skate();
     }
-    
+
 }
