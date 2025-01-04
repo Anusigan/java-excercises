d1cd455909658b86b66c23f5ab73ed2d3d77f7c4
Anusigan Sivananthan <148673260+Anusigan@users.noreply.github.com>
2025-01-04 15:28:39 +0530

    interface study

--- AbstractTwo.java
+++ AbstractTwo.java
@@ -2,10 +2,24 @@ interface A {
     void show();
 
     void config();
+
+}
+
+class B implements A {
+    public void show() {
+        System.out.println("How are you?");
+    }
+
+    public void config() {
+        System.out.println("Fine thats all");
+    }
 }
 
 public class AbstractTwo {
-    A sc = new A();
-    
+    public static void main(String[] args) {
+        A sc = new B();
+        sc.show();
+
+    }
 
 }
