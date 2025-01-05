c780616fdd7ebb1cc9400b68df6a69c0acbaca4a
Anusigan Sivananthan <148673260+Anusigan@users.noreply.github.com>
2025-01-05 15:13:12 +0530

    exception

--- ExceptionA.java
+++ ExceptionA.java
@@ -1,25 +1,37 @@
 
+class MyException extends Exception{
 
+    public MyException(String anu){
+        super(anu);
+
+    }
+}
 public class ExceptionA {
     public static void main(String[] args) {
-        int i=0;
-        
+        int i=20;
         int j=0;
+
         try{
          j=18/i;
-         
+         if(j==0){
+            
+           throw new MyException("hi");
+         }
+
 
 
        
         
         }
-        catch (ArithmeticException e){
-            System.out.println(e);
+        catch (MyException e){
+            System.out.println("custom"+e);
         }
-        catch(ArrayIndexOutOfBoundsException e){
-            System.out.println(e);
-        }
-        System.out.println(j);
+       
+       catch(Exception e){
+        System.out.println("ll");
+       }
+       
+        
 
     }
     
