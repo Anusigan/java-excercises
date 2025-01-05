db22f6d447af80cf020f4e6319d38a6c3b26315f
Anusigan Sivananthan <148673260+Anusigan@users.noreply.github.com>
2025-01-05 15:37:58 +0530

    threads

---
+++ Thr.java
@@ -0,0 +1,31 @@
+class A{
+   public void show(){
+    for (int i=0;i<=10;i++){
+    System.out.println("Hi");
+    }
+   }
+
+}
+class B{
+    public void show(){
+    for (int i=0;i<=10;i++){
+        System.out.println("Hello");
+        }
+       }
+    
+
+}
+
+
+public class Thr {
+    public static void main(String[] args) {
+    A sc=new A();
+    B sc1=new B();
+    sc.show();
+    sc1.show();
+        
+    }
+    
+
+    
+}
