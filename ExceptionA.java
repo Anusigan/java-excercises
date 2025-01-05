a9cf6dcae1e8b36777c52281dcab3dfb80cc1ace
Anusigan Sivananthan <148673260+Anusigan@users.noreply.github.com>
2025-01-05 13:11:45 +0530

    exception handling

--- ExceptionA.java
+++ ExceptionA.java
@@ -3,9 +3,11 @@
 public class ExceptionA {
     public static void main(String[] args) {
         int i=0;
+        int nums[]=new int[5];
         try{
-        
         int j=18/i;
+        System.out.println(nums[1]);
+        System.out.println(nums[5]);
         System.out.println(j);
         }
         catch (Exception e){
