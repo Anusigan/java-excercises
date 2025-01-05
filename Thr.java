a50dbebf515e99e008cdbd0f41e851e02bc4a938
Anusigan Sivananthan <148673260+Anusigan@users.noreply.github.com>
2025-01-05 15:59:09 +0530

    changes threads

--- Thr.java
+++ Thr.java
@@ -1,31 +1,30 @@
-class A extends Thread{  //now this became thread
-   public void show(){
-    for (int i=0;i<=10;i++){
-    System.out.println("Hi");
-    }
-   }
-
-}
-class B extends Thread{  //now this became thread
-    public void show(){
-    for (int i=0;i<=10;i++){
-        System.out.println("Hello");
+class A extends Thread { // now this became thread
+    public void run() {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("Hi");
+            Thread.sleep(10);
         }
-       }
-    
+    }
 
 }
 
+class B extends Thread { // now this became thread
+    public void run() {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("Hello");
+
+        }
+    }
+
+}
 
 public class Thr {
     public static void main(String[] args) {
-    A sc=new A();
-    B sc1=new B();
-    sc.show();
-    sc1.show();
-        
-    }
-    
+        A sc = new A();
+        B sc1 = new B();
+        sc.start();
+        sc1.start();
 
-    
+    }
+
 }
