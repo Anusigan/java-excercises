1f191104ff9c4e80d4a594244721164bb3b0c1f2
Anusigan Sivananthan <148673260+Anusigan@users.noreply.github.com>
2025-01-06 23:31:35 +0530

    abstract class

--- Prac2.java
+++ Prac2.java
@@ -1,14 +1,23 @@
-abstract class Donkey{
+abstract class Donkey {
     public abstract void running();
+
     public abstract void singing();
-    public void anu(){
+
+    public void anu() {
         System.out.println("hi");
     }
 
-    
 }
 
+class B extends Donkey{
+    public void running(){
+        int i=0;
+        int j=2;
+        System.out.println(i+j);
+    }
+    pu
+}
 
 public class Prac2 {
-    
+
 }
