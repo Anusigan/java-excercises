7a0903a117fb020181e7a75046b55852ae7e4fa5
Anusigan Sivananthan <148673260+Anusigan@users.noreply.github.com>
2025-01-07 18:48:39 +0530

    changes

--- ArrayL.java
+++ ArrayL.java
@@ -3,7 +3,9 @@ import java.util.ArrayList;
 public class ArrayL {
     public static void main(String[] args) {
         ArrayList<String> cars = new ArrayList<String>();
-        
+        cars.add(4,"benz");
+        cars.add("BMW");
+        System.out.println(cars);
     }
 
     
