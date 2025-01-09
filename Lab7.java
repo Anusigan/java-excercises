cf637f9684659d07b64da87a80934511555faa44
Anusigan Sivananthan <148673260+Anusigan@users.noreply.github.com>
2025-01-10 01:02:08 +0530

    lab

---
+++ Lab7.java
@@ -0,0 +1,26 @@
+abstract class Vehicle{
+     int speed;
+    String colour;
+
+    public Vehicle(){
+        speed=30;
+        colour="Blue";
+       
+    }
+}
+
+class Car extends Vehicle{
+    
+    public void print(){
+
+        System.out.println(speed);
+    }
+
+}
+
+
+class 
+public class Lab7 {
+
+    
+}
