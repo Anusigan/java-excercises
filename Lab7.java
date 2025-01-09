5dc5b67dad2683c01c984e2e0189b39a3e312d8b
Anusigan Sivananthan <148673260+Anusigan@users.noreply.github.com>
2025-01-10 01:21:09 +0530

    lab

--- Lab7.java
+++ Lab7.java
@@ -11,7 +11,7 @@ abstract class Vehicle{
 
 class Car extends Vehicle{
     
-    public void print(){
+    public void printa(){
 
         System.out.println(speed);
     }
@@ -20,11 +20,18 @@ class Car extends Vehicle{
 
 
 class Bike extends Vehicle{
-    public void print(){
+    public void printa(){
         System.out.println(colour);
     }
 }
 public class Lab7 {
+    public static void main(String[] args) {
+        Car car=new Car();
+        Bike bike=new Bike();
+        car.printa();
+        bike.printa();
+
+    }
 
     
 }
