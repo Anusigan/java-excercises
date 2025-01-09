47f50315016db77703250cff32505d4d9dae7855
Anusigan Sivananthan <148673260+Anusigan@users.noreply.github.com>
2025-01-09 23:05:57 +0530

    "Checkinggg"

--- Lab.java
+++ Lab.java
@@ -17,7 +17,7 @@ public class Lab {
         System.out.println("Checkinggg");
         System.out.println("Checkinggg");
         System.out.println("Checkinggg");
-        System.out.println();
+        System.out.println("Checkinggg");
         System.out.println();
     }
 
