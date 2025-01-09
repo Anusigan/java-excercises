f0a364e2f08f4f855042d15efbf578531227d70b
Anusigan Sivananthan <148673260+Anusigan@users.noreply.github.com>
2025-01-10 00:51:10 +0530

    Lab test

--- Lab6.java
+++ Lab6.java
@@ -18,5 +18,10 @@ class Duck implements Flyable, Swimmable {
 }
 
 public class Lab6 {
+    public static void main(String[] args) {
+        Duck d=new Duck();
+        d.fly();
+        d.swim();
+    }
 
 }
