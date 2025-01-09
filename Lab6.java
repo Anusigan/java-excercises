ad34570b8f76add397f17d9ef22d4838bc5505d2
Anusigan Sivananthan <148673260+Anusigan@users.noreply.github.com>
2025-01-10 00:21:50 +0530

    lab test

---
+++ Lab6.java
@@ -0,0 +1,18 @@
+interface Flyable {
+    void fly();
+}
+
+interface Swimmable {
+    void swim();
+}
+
+class Duck implements Flyable, Swimmable {
+    public void fly() {
+        System.out.println("I can swim");
+    }
+
+}
+
+public class Lab6 {
+
+}
