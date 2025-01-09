cbb6cd4988d536ec9dc917ff8a0fc90a32a7b433
Anusigan Sivananthan <148673260+Anusigan@users.noreply.github.com>
2025-01-09 23:06:38 +0530

    "Checkinggg"

--- Lab.java
+++ Lab.java
@@ -1,7 +1,7 @@
 public class Lab {
 
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("Checkinggg");
         System.out.println("Checkinggg");
         System.out.println("Checkinggg");
         System.out.println("Checkinggg");
