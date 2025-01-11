28c95bacbb8e548043bb7dac7975c7db343922a4
Anusigan Sivananthan <148673260+Anusigan@users.noreply.github.com>
2025-01-11 18:53:10 +0530

    changes

--- Lab11.java
+++ Lab11.java
@@ -4,7 +4,18 @@ interface A{
 }
 
 
+class B implements A{
+    public void fly(){
+        System.out.println("Im flying");
+    }
+    public void jump(){
+        System.out.println("Anusigan");
+    }
+}
+
+
 
 public class Lab11 {
+
     
 }
