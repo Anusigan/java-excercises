37fc2e97c86182b6691d3353b092aacf145df97e
Anusigan Sivananthan <148673260+Anusigan@users.noreply.github.com>
2025-01-12 23:54:37 +0530

    changes

--- Lab20.java
+++ Lab20.java
@@ -1,33 +1,31 @@
-class Student{
-	private String name;
-	private int age;
-	private int height;
+class Student {
+    private String name;
+    private int age;
+    private int height;
 
-	public  Student(){
-	this.name=name;
-	this.age=age;
-	this.height=height;
+    public Student() {
+        this.name = name;
+        this.age = age;
+        this.height = height;
 
-	}
+    }
 
-	public String getName(){
-	return name;
-	}
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public void setName(String name){
-	this.name=name;
-	}
+    public int getAge() {
+        return age;
+    }
 
-	public int getAge(){
-	return age;
-	}
-
-	public void  setAge(int age){
+    public void  setAge(int){
 	this.age=age;
 	}
 }
-
 
 public class Lab20{
 	public static void main(String[] args){
