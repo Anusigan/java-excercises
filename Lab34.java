ab9930291326b16c9a5b9ac034591a062d86dab4
Anusigan Sivananthan <148673260+Anusigan@users.noreply.github.com>
2025-01-13 00:36:43 +0530

    change

--- Lab34.java
+++ Lab34.java
@@ -10,7 +10,7 @@ class A implements Runnable {
 
 class B implements Runnable {
     public void run() {
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 10; i++) {
             System.out.println("Hi");
         }
     }
