430ae16cfaedfdb35c0064beba6e1f067109c4b8
Anusigan Sivananthan <148673260+Anusigan@users.noreply.github.com>
2025-03-24 21:49:04 +0530

    question try

---
+++ int1.java
@@ -0,0 +1,11 @@
+public class int1{
+    public static void main(String [] args){
+        int [] numbers=new int [10];
+        for(int i=0;i<10;i++){
+            numbers[i]=i;
+        }
+        for(int nanu:numbers){
+            System.out.println(nanu);
+        }
+    }
+}
