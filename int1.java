53c65d667462b8d1b576d2e0ac9083979c2fef59
Anusigan Sivananthan <148673260+Anusigan@users.noreply.github.com>
2025-03-24 23:17:08 +0530

    updated

--- int1.java
+++ int1.java
@@ -1,8 +1,17 @@
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.HashSet;
 public class int1{
-    public static void main(String[] args) {
-      ArrayList<Integer> anu=new ArrayList<Integer>();
-      
+    public static void main(String[] args){
+       ArrayList <Integer> bus = new ArrayList <Integer>();
+       bus.add(1);
+         bus.add(2);
+        bus.add(2);
+        bus.add(3);
+        bus.add(4);
+        ArrayList < Integer> hi =new ArrayList <Integer>(new HashSet (bus));
+        for (int num: hi){
+            System.out.println(num);
+        }
     }
+
 }
