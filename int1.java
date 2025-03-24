d46137ff3fe076cee801f062ea5873afc302658f
Anusigan Sivananthan <148673260+Anusigan@users.noreply.github.com>
2025-03-24 21:57:57 +0530

    updated

--- int1.java
+++ int1.java
@@ -1,18 +1,8 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 public class int1{
-    public static void main(String [] args){
-      int [] num =new int []{10,40,90,60,500,100};
-      int large = num[0];
-      for(int i : num){
-        if(i>large){
-            large = i;
-        }
-        else{
-            large=large;
-        }
-        
-    
-
-      }
-      System.out.println(large);
+    public static void main(String[] args) {
+      ArrayList<Integer> anu=new ArrayList<Integer>();
+      
     }
 }
